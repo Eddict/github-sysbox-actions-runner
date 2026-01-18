a410dc245c4b6e0deeb824fa3d5d2d5c08e90339
Eddict <14122921+Eddict@users.noreply.github.com>
2026-01-19 00:37:14 +0100

    added arg "--ulimit nofile=1048576:524288" to prevent error "/etc/init.d/docker: 61: ulimit: error setting limit (Operation not permitted)"

--- gha_runner_create.sh
+++ gha_runner_create.sh
@@ -20,6 +20,7 @@ function create_sysbox_gha_runner {
 
     docker run -d --restart=always \
         --runtime=sysbox-runc \
+        --ulimit nofile=1048576:524288 \
         -e REPO_URL="https://github.com/${org}/${repo}" \
         -e RUNNER_TOKEN="$token" \
         -e RUNNER_NAME="$name" \
